4064c500818669389057ddd109c3effa12a7d5e3
quan91 <quan91.nt@gmail.com>
2023-02-24 20:11:58 +0700

    add console project

---
+++ CSharpCoban/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCoban
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chào mừng các bạn đến với khóa học ASPNET MVC");
+            Console.ReadLine();
+        }
+    }
+}
