fe52d08c9a68cb519dc6daa54609841990155755
quan91 <quan91.nt@gmail.com>
2023-04-21 21:00:42 +0700

    aspnet ,aspnet core

---
+++ WebApplicationDemo/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplicationDemo
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               name: "my-nam-quan",
+               url: "my-nam-quan",
+               defaults: new { controller = "Home", action = "About", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+              name: "MY_URL",
+              url: "chi-tiet-{name}-{id}",
+              defaults: new { controller = "Home", action = "Detail", name= UrlParameter.Optional, id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplicationDemo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationDemo.Models;
+
+namespace WebApplicationDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new List<ProductModels>();
+            for (int i = 0; i < 10; i++)
+            {
+                model.Add(new ProductModels { Id = i, Name = "Điện thoại số " + i });
+            }
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //Kiểm tra quyền người 
+
+            return RedirectToAction("Login", "Home");
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Detail(int? id)
+        {
+            ViewBag.ID = id;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
